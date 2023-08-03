5ba535e9586dd8b61da7d1f8e8bd13115ff65dac
simminjeonghome <pooh5045@naver.com>
2023-08-03 15:44:51 +0900

    init

---
+++ spring/src/test/java/com/kopo/SelfFDS/SelfFdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kopo.SelfFDS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfFdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
